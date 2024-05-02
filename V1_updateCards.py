8df0d46de097608f6303c98e4cb868c923753c78
wudthedud <wud@middleton.school.nz>
2024-05-02 15:13:42 +1200

    updated data stprage

--- V1_updateCards.py
+++ V1_updateCards.py
@@ -3,6 +3,18 @@ saves card details in a persistent text file'''
 import shelve
 
 def updateCards(name, stats):
-    d = shelve.open('cards')  # here you will save the score variable
-    d['highscores_numbers'] = [["Daniel", 100], ["Name2", 90], ["Name3", 80], ["Name4", 70], ["Name5", 60]]
+    '''updates text file with card data'''
+    d = shelve.open('cards.txt')
+    data = d['cards']
+    data[name] = stats
+    d['cards'] = data
     d.close()
+
+def returnCards():
+    '''returns card data from text file'''
+    d = shelve.open('cards.txt')
+    data = d['cards']
+    d.close()
+    for name, stats in data.items():
+        print(f"{name}, {stats}\n")
+returnCards()
