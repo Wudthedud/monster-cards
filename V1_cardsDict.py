63b6df6033684ef985db90d9eb308f0adcf44679
wudthedud <wud@middleton.school.nz>
2024-05-09 14:38:15 +1200

    9.5

---
+++ V1_cardsDict.py
@@ -0,0 +1,12 @@
+'''dictonary of cards'''
+
+cards = {'stoneling': [7, 1, 25, 15],
+         'vexscream': [1, 6, 21, 19],
+         'dawnmirage': [5, 15, 18, 22],
+         'blazegolem': [15, 20, 23, 6],
+         'websnake': [7, 15, 10, 5],
+         'moldvine': [21, 18, 14, 5],
+         'vortexwing': [19, 13, 19, 2],
+         'rotthing': [16, 7, 4, 12],
+         'froststep': [14, 14, 17, 4],
+         'wispgoul': [17, 19, 3, 2]}
