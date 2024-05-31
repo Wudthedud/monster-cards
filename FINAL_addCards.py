56e4de15fe6c8bc83cea182b84cf2c3898b0a8a3
wudthedud <wud@middleton.school.nz>
2024-06-01 04:23:54 +1200

    PEP8

--- FINAL_addCards.py
+++ FINAL_addCards.py
@@ -1,16 +1,19 @@
-"""FINAL_addCards.py
-adds card to dictionary"""
+"""FINAL_addCards.py.
+
+adds card to dictionary
+"""
 import shelve
 import easygui as eg
 
 
 def add_card(name):
-    """added GUI functionality to adding cards"""
+    """Asks for name and stats, adds to dictionary once confirmed."""
     d = shelve.open("cards.txt")
     data = d["cards"]
     try:
         if name in data:
-            eg.msgbox(f"The card '{name.capitalize()} already exists", "Cancelled add card")
+            eg.msgbox(f"The card '{name.capitalize()} already exists",
+                      "Cancelled add card")
             return
         stats = []
         key = ["strength", "speed", "stealth", "cunning"]
@@ -24,8 +27,10 @@ def add_card(name):
                 break
             stats.append(stat)
 
-        if eg.ynbox(f"Are all the details correct?\n\n---{name.capitalize()}---\nStrength: {stats[0]}\n"
-                    f"Speed: {stats[1]}\nStealth: {stats[2]}\nCunning: {stats[3]}", "Add card"):
+        if eg.ynbox(f"Are all the details correct?"
+                    f"\n\n---{name.capitalize()}---"
+                    f"\nStrength: {stats[0]}\nSpeed: {stats[1]}\nStealth: "
+                    f"{stats[2]}\nCunning: {stats[3]}", "Add card"):
             data[name] = stats
             d["cards"] = data
             d.close()
@@ -33,8 +38,8 @@ def add_card(name):
         else:
             choice = eg.ynbox("Would you like to try again?", "Add card")
             if choice:
-                name = eg.enterbox("What is the name of the card you would like to add?",
-                                   "Add card")
+                name = eg.enterbox("What is the name of the card "
+                                   "you would like to add?", "Add card")
                 add_card(name)
             else:
                 return
--- FINAL_monsterCards.py
+++ FINAL_monsterCards.py
@@ -1,26 +1,32 @@
-"""FINAL_monsterCards.py
-main program that calls other functions, welcome message and string checker"""
+"""FINAL_monsterCards.py.
+
+main program that calls other functions, welcome message and string checker
+"""
 import sys
 import easygui as eg
 from FINAL_addCards import add_card
 from FINAL_printCards import print_gui
 from FINAL_searchCards import search_card
 from FINAL_removeCard import remove_card
-from V2_cardsDict import reset
+from FINAL_cardsDict import reset
 
 
 def welcome():
-    """welcome message"""
+    """Welcome message, and option to reset catalogue."""
     msg = (
         "Welcome to the Monster Card Game Catalogue!\n"
         "This program allows you to manage a collection of monster cards.\n\n"
-        "Menu Options:\nAdd a new monster card: Create a new card and assign statistics to it.\n"
-        "Search for a monster card: Find an existing card and verify its details,"
-        "with the option to edit or remove if required.\nDelete a monster card: "
-        "Remove a card from the catalogue.\nExit: Quit the program.\n\n"
-        "Note: Any changes made to the catalogue will apply even when the program is closed."
-        "If you wish to reset the catalogue to it\"s default state, press the 'reset' button below.")
-    welcome_choice = eg.buttonbox(msg, "Welcome", ["Continue", "Reset catalogue", "Exit"])
+        "Menu Options:\nAdd a new monster card: "
+        "Create a new card and assign statistics to it.\n"
+        "Search for a monster card: Find an existing card and "
+        "verify its details, with the option to edit or remove if required.\n"
+        "Delete a monster card: Remove a card from the catalogue.\n"
+        "Exit: Quit the program.\n\n"
+        "Note: Any changes made to the catalogue will apply even when the "
+        "program is closed. If you wish to reset the catalogue to it's "
+        "default state, press the 'reset' button below.")
+    welcome_choice = eg.buttonbox(msg, "Welcome",
+                                  ["Continue", "Reset catalogue", "Exit"])
     if welcome_choice == "Exit":
         sys.exit()
     elif welcome_choice == "Reset catalogue":
@@ -31,35 +37,41 @@ def welcome():
 
 
 def input_check(question):
-    """template for questions in main program"""
+    """Check name input is valid and not empty."""
     while True:
-        user_input = eg.enterbox(f"What is the name of the card you would like to {question}?",
+        user_input = eg.enterbox(f"What is the name of the card you would"
+                                 f" like to {question}?",
                                  f"{question.capitalize()} card")
         if user_input == "":
             retry = eg.ynbox("This field cannot be empty\nTry again?",
                              f"{question.capitalize()} card")
             if not retry:
-                eg.msgbox("Operation cancelled", f"{question.capitalize()} card")
+                eg.msgbox("Operation cancelled",
+                          f"{question.capitalize()} card")
                 return None
         elif user_input is None:
-            eg.msgbox("Operation cancelled", f"{question.capitalize()} card")
+            eg.msgbox("Operation cancelled",
+                      f"{question.capitalize()} card")
             return None
         elif not user_input.isalpha():
-            retry = eg.ynbox("This field can only contain alphabetic characters\nTry again?",
+            retry = eg.ynbox("This field can only contain alphabetic "
+                             "characters\nTry again?",
                              f"{question.capitalize()} card")
             if not retry:
-                eg.msgbox("Operation cancelled", f"{question.capitalize()} card")
+                eg.msgbox("Operation cancelled",
+                          f"{question.capitalize()} card")
                 return None
         else:
             return user_input.lower().strip()
 
 
 def main():
-    """main menu"""
+    """Display main menu."""
     while True:
         choice = eg.buttonbox("Welcome to Monster Cards Catalogue\n"
                               "What would you like to do?", "Welcome",
-                              ["Add card", "Remove card", "Search for card", "Print cards", "Exit"])
+                              ["Add card", "Remove card",
+                               "Search for card", "Print cards", "Exit"])
         if choice == "Add card":
             name = input_check("add")
             if name is not None:
