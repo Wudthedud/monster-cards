3876fedfd0935a9a43f812ba5284481b5af84cbd
wudthedud <wud@middleton.school.nz>
2024-06-02 00:13:37 +1200

    Update FINAL_removeCard.py

--- FINAL_removeCard.py
+++ FINAL_removeCard.py
@@ -1,30 +1,34 @@
-"""FINAL_removeCards.py
-removes selected card, adds error checking"""
+"""FINAL_removeCards.py.
+
+removes selected card, adds error checking
+"""
 import shelve
 import easygui as eg
 
 
 def remove_card(name=None):
-    """adds easyGUI to the remove card function"""
+    """Ask for card to remove and removes it if confirmed."""
     d = shelve.open("cards.txt")
     data = d["cards"]
 
     while True:
         if name is None:
             keys = [key.title() for key in data.keys()]
-            name = eg.choicebox("Which card would you like to remove?", "Remove card", keys)
+            name = eg.choicebox("Which card would you like to remove?",
+                                "Remove card", keys)
             if name is None:
                 eg.msgbox("Operation cancelled", "Remove card")
                 d.close()
                 return
-        choice = eg.ynbox(f"Are you sure you want to remove '{name.capitalize()}'?", "Remove card")
+        choice = eg.ynbox(f"Are you sure you want to remove "
+                          f"'{name.capitalize()}'?", "Remove card")
         if choice:
             del data[name.lower()]
             d["cards"] = data
-            eg.msgbox(f"Card '{name.capitalize()}' removed", "Remove card")
+            eg.msgbox(f"Card '{name.capitalize()}' removed",
+                      "Remove card")
             d.close()
             return
         else:
             eg.msgbox("Operation cancelled", "Remove card")
             return
-        
